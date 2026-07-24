3ffca87840231e63b8ae2b9197e514a77ea20faf
Teo Wei Han <dextertwh@gmail.com>
2026-07-24 21:38:06 +0800

    1

--- routes/submission.js
+++ routes/submission.js
@@ -3,13 +3,21 @@ const router = express.Router();
 
 const db = require("../config/db");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 
 // Image upload configuration
+const uploadDir = path.join(__dirname, "../public/uploads/certificates");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
 
     destination: (req, file, cb) => {
-        cb(null, "public/uploads/certificates");
+        cb(null, uploadDir);
     },
 
     filename: (req, file, cb) => {
